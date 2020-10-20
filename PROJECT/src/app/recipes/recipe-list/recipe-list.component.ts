c44be185d3ec2f2a52bae9ea92fc700bcf3570e1
PhoenixInvertigo <60861253+PhoenixInvertigo@users.noreply.github.com>
2020-10-20 14:54:50 -0500

    Components and Databinding
    Implemented Section 6: Course Project - Components and Databinding

--- PROJECT/src/app/recipes/recipe-list/recipe-list.component.ts
+++ PROJECT/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model'
 
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes: Recipe[] = [
   new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
 ];
@@ -16,5 +17,8 @@ recipes: Recipe[] = [
 
   ngOnInit(): void {
   }
-
+  
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
